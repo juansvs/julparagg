27b9b81ec79682865a28d9a4252e3848c09e1bf7
juansvs <juans.vs@gmail.com>
2025-11-24 13:41:13 +0100

    changed parameters to Dict struct, to be used with paramscan function. Added paramscan function call and output (agent df, model df)

--- sheep-parasite-model/src/main.jl
+++ sheep-parasite-model/src/main.jl
@@ -1,38 +1,44 @@
 # src/main.jl
 
-using Agents, Random, Base.Threads, StatsBase, JLD2, Plots
-using Distributions:Poisson
-using Distributions:Multinomial
+using Agents, Random, Base.Threads, StatsBase, JLD2, Plots, Distributions
 
 include("agents.jl")
 include("space.jl")
 include("simulation.jl")
 include("model.jl")
-include("visualization.jl")
+#include("visualization.jl")
 
 # set simulation parameters
-params = Dict(
-    "gamma" => 0.00004,
-    "max_sward_height" => 400,
-    "epsilon" => 0.00005,
-    "mu_l" => 0.0001,
-    "mu_L" => 0.000015,
-    "phi" => 0.00001776,
-    "beta" => 0.1,
-    "min_sward_height" => 50,
-    "mu_k" => 0.0,
-    "Lambda" => 1.0,
-    "mu_a" => 0.01,
-    "mu_A" => 2e-5,
-    "chi" => 0.00003,
-    "eta" => 0.025,
-    "sigma" => 1.9e-8,
-    "lambda" => 2.0,
-    "s0" => 2000,
-    "fdep" => 1,
-    "nu" => 0.015,
-    "inf_prob" => 0.4
+parameters = Dict(
+    :num_sheep => [10, 30],
+    :arena_side => [78,100],
+    :seed => collect(1:5),
+    :gamma => 0.00004,
+    :max_sward_height => 400,
+    :epsilon => 0.00005,
+    :mu_l => 0.0001,
+    :mu_L => 0.000015,
+    :phi => 0.00001776,
+    :beta => 0.1,
+    :min_sward_height => 50,
+    :mu_k => [0.0, 0.1],
+    :Lambda => [0.0, 0.1],
+    :mu_a => 0.01,
+    :mu_A => 2e-5,
+    :chi => 0.00003,
+    :eta => 0.025,
+    :sigma => 1.9e-8,
+    :lambda => 2.0,
+    :s0 => 2000,
+    :fdep => 1,
+    :nu => 0.015,
+    :inf_prob => 0.4
 )
+
+adf, mdf = paramscan(parameters, initialize_model; showprogress = true, 
+    n = 60*24*10.0, when = 360.0,
+    adata = [:imm_par_load, :mat_par_load, :stomach_content, :immunity_level],
+    mdata = [:sward_height, :inf_larva_number, :uninf_larva_number])
 
 function main()
     # Initialize the model
@@ -42,8 +48,6 @@ function main()
     simtime = 60*24*15.0  # total simulation time in minutes
     time_series =     run_sim!(model, simtime, 360.0)
 
-    # Visualize the results
-    # visualize_results(model)
     return model, time_series
 end
 
