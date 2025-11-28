8809a5ce0ce3f02defb8807939f4e78432ff5adb
juansvs <juans.vs@gmail.com>
2025-11-28 16:33:17 +0100

    changes to main to create multiple parameter scenarios

--- sheep-parasite-model/src/main.jl
+++ sheep-parasite-model/src/main.jl
@@ -12,10 +12,10 @@
 end
 
 # set simulation parameters
-@everywhere params = Dict(
-    "Na" => 5,
-    "arena_side" => 78,
-    "simtime" => 24*60*15.0,
+params_list = [Dict(
+    "Na" => 10,
+    "arena_side" => 100,
+    "simtime" => 24*60*60.0,
     "gamma" => 0.00004,
     "max_sward_height" => 400,
     "epsilon" => 0.00005,
@@ -24,8 +24,8 @@ end
     "phi" => 0.00001776,
     "beta" => 0.1,
     "min_sward_height" => 50,
-    "mu_k" => 0.0,
-    "Lambda" => 1.0,
+    "mu_k" => mu_k,
+    "Lambda" => Lambda,
     "mu_a" => 0.0001,
     "mu_A" => 2e-5,
     "chi" => 0.00003,
@@ -36,7 +36,9 @@ end
     "fdep" => 1,
     "nu" => 0.015,
     "inf_prob" => 0.4
-)
+) 
+for mu_k in [0.0 0.5 1.0] for Lambda in [0.0 0.5 1.0]
+]
 
 
 @everywhere function main(params)
@@ -55,6 +57,6 @@ end
 # @time endstate, time_series = main()
 
 # run in parallel
-@time c = pmap(_ -> main(params), 1:8)
+@time c = pmap(main, params_list)
 # write outputs to file
-@save "simulation_output.jld2" c
+@save "simulation_output.jld2" c params_list
