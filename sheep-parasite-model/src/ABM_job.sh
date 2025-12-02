c2f35804f0196b3cb81519bae013a93eba3781e5
juansvs <juans.vs@gmail.com>
2025-12-02 16:10:41 +0000

    increasing ntasks, editing out request for machinefile in bash script

--- sheep-parasite-model/src/ABM_job.sh
+++ sheep-parasite-model/src/ABM_job.sh
@@ -4,12 +4,14 @@
 #SBATCH --output=abm-%J.out
 #SBATCH --error=abm-%J.err
 #SBATCH --time=24:00:00
-#SBATCH --ntasks=10
+#SBATCH --ntasks=20
 #SBATCH --mem-per-cpu=2G
 #SBATCH --nodes=1-4
 #SBATCH --partition=k2-medpri
 #SBATCH --mail-type=ALL
+#SBATCH --mail-user=j.vargas@qub.ac.uk
 
+cd ~/julparagg
 module load apps/julia/1.11.2
 
-julia --machinefile=$SLURM_NODEFILE ~/julparagg/sheep-aggregation/src/main.jl
+julia sheep-parasite-model/src/main.jl
